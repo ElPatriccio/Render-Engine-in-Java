5a75a05578d4710cc8175d6a8e6191c2fdf9c9cf
ElPatriccio <patrick.jktsh@gmail.com>
2023-12-30 18:19:33 +0100

    Vector package

---
+++ src/main/java/Vector/Vector.java
@@ -0,0 +1,40 @@
+package Vector;
+
+import java.awt.geom.QuadCurve2D;
+import java.util.Iterator;
+
+public class Vector implements Iterable<Float>{
+    private final float[] v;
+
+    public Vector(float ...values){
+        v = values;
+    }
+
+    public Vector(int dim){
+        v = new float[dim+1];
+    }
+
+    public float get(int index){
+        return v[index];
+    }
+    public void set(int index, float value){
+        v[index] = value;
+    }
+
+    public int dimension(){
+        return v.length - 1;
+    }
+
+    public int size(){
+        return v.length;
+    }
+
+    @Override
+    public Iterator<Float> iterator() {
+        return new VecIter(v, false);
+    }
+
+    public Iterator<Float> homogenizedIterator(){
+        return new VecIter(v, true);
+    }
+}
