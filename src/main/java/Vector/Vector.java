2bb17de3b4310383f6dce4cbfe5b582307bc6374
ElPatriccio <patrick.jktsh@gmail.com>
2024-01-01 18:42:56 +0100

    merged VecIter into Vector

--- src/main/java/Vector/Vector.java
+++ src/main/java/Vector/Vector.java
@@ -176,3 +176,25 @@ public class Vector implements Iterable<Float>{
         return Arrays.toString(v);
     }
 }
+
+class VecIter implements Iterator<Float> {
+    private final float[] v;
+    private int i = 0;
+    private final int x;
+
+
+    public VecIter(float[] v, boolean homogenized){
+        this.v = v;
+        x = homogenized ? 0 : 1;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return i < v.length - x;
+    }
+
+    @Override
+    public Float next() {
+        return v[i++];
+    }
+}
