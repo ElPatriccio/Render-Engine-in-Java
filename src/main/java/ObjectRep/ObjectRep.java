84fae3c7ce29350cfe6b50589f7ad0ffe73bbecf
ElPatriccio <patrick.jktsh@gmail.com>
2024-01-02 16:16:34 +0100

    ObjectRep interface

--- src/main/java/ObjectRep/ObjectRep.java
+++ src/main/java/ObjectRep/ObjectRep.java
@@ -7,7 +7,14 @@ import Window.Window;
 
 
 public interface ObjectRep extends Iterable<Vector> {
+
+    Vector get(int index);
+
+    void set(int index, Vector v);
+
     void applyTransformation(Matrix transform);
+
+    ObjectRep createNewObjectWithTransform(Matrix transform);
 
     int amountOfVertices();
 
